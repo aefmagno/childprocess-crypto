e993cc77a89933fd5af45b40560824fdafbc3c61
Aaron Magno <aefmagno@gmaik.com>
2023-01-10 13:16:01 +0800

    used external property of memoryUsage instead of heapUsed

--- fork/parent.js
+++ fork/parent.js
@@ -1,33 +1,12 @@
-import crypto from 'node:crypto'
 import { fork } from 'node:child_process'
-import { readFileSync } from 'node:fs'
 
-const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
-  modulusLength: 2048,
-  publicKeyEncoding: {
-    type: 'spki',
-    format: 'pem',
-  },
-  privateKeyEncoding: {
-    type: 'pkcs8',
-    format: 'pem',
-  },
-})
-
-function verifyData(data) {
-  const buffer = Buffer.from(data, 'base64')
-  const algo = 'SHA256'
-
-  const signature = crypto.sign(algo, buffer, privateKey)
-  return crypto.verify(algo, buffer, publicKey, signature)
-}
+import {
+  publicKey,
+  verifyData,
+  file,
+} from '../helper.js'
 
 const child = fork('./child.js')
-
-const file = readFileSync('../file.txt', {
-  encoding: 'utf8',
-  flag: 'r',
-})
 
 child.send({ publicKey, file })
 
@@ -37,13 +16,17 @@ child.on('message', (data) => {
   const verify = verifyData(data)
   if (verify) {
     result.verify = 'The signature is verified'
-    result.memoryUsage = process.memoryUsage().heapUsed
+    result.memoryUsage = process.memoryUsage().external
 
     process.kill(child.pid, 'SIGHUP')
     result.executionTime = process.uptime()
 
     console.table(result)
   } else {
-    throw new Error('The signature is not verified')
+    process.kill(child.pid, 'SIGHUP')
+    result.verify = 'The signature is verified'
+    result.memoryUsage = process.memoryUsage().external
+    result.executionTime = process.uptime()
+    console.table(result)
   }
 })
